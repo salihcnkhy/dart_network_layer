7256e0a287f57a61a412de4ed1fcc36adf180084
Salihcan Kahya <salihcankahya@gmail.com>
2022-01-21 23:36:20 +0300

    mock class

--- lib/network_layer.dart
+++ lib/network_layer.dart
@@ -1,1 +1,12 @@
 library network_layer;
+
+import 'interfaces/network_manager_interface.dart';
+import 'mock/mock_options_provider.dart';
+import 'network_manager.dart';
+
+class NetworkLayer {
+  static NetworkManagerInterface networkManager =
+      NetworkManager(MockOptionsProvider());
+
+  static String baseUrl = "https://hesabinibil.azurewebsites.net/api";
+}
